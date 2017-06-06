bade1b2c0a4f287fb14efeb8324a4846989c343d
Jared Graham <email": null>
2017-06-06 10:04:28 -0700

    Adjust regex to show markdown symbols if not at the beginning of the line

--- js/application.js
+++ js/application.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-  $("#source").on('keyup', function(event){
-    input = $('#source').val()
-    $('#preview').empty()
-    $('#preview').append(transform(input))
-  })
+  MarkdownWidget("#source-id", "#preview-div")
 });
 
 function transform(input) {
   var lines = input.split("\n")
   for(var i = 0; i < lines.length; i++){
-    if(lines[i].match(/^(\s*)[_]|\*{2}/)){
-      lines[i] = "<p>" + "<em>" + lines[i].replace(/^(\s*)[_]|\*{2}?/g, '$1') + "</em>" + "</p>"
+    if(lines[i].match(/^(\s*)([_]|\*{2})/)){
+      lines[i] = "<p>" + "<em>" + lines[i].replace(/^(\s*)[_]|\*{2}/g, '$1') + "</em>" + "</p>"
     }
     else if(lines[i].match(/^(\s*)\*{1}/)){
-      lines[i] = "<p>" + "<strong>" + lines[i].replace(/^(\s*)[\*_]\*?/g, '$1') + "</strong>" + "</p>"
+      lines[i] = "<p>" + "<strong>" + lines[i].replace(/^(\s*)\*{1}/g, '$1') + "</strong>" + "</p>"
     }
     else{
       lines[i] = "<p>" + lines[i] + "</p>"
@@ -21,4 +17,12 @@ function transform(input) {
   }
   console.log("lines are " + lines.join('\n'))
   return lines.join('\n');
+}
+
+function MarkdownWidget(inputID, outputID){
+  $(inputID).on('keyup', function(event){
+    input = $(inputID).val()
+    $(outputID).empty()
+    $(outputID).append(transform(input))
+  })
 }
