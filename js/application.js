cba4a058389126315fd4f3230512eb7c2cbd4ad8
Jared Graham <email": null>
2017-06-06 09:41:17 -0700

    Change conditional for checking markdown symbols to regex

--- js/application.js
+++ js/application.js
@@ -9,15 +9,16 @@ $(document).ready(function() {
 function transform(input) {
   var lines = input.split("\n")
   for(var i = 0; i < lines.length; i++){
-    if(lines[i].trim().substring(0, 2) === '**' || lines[i].trim()[0] === '_'){
-      lines[i] = "<p>" + "<em>" + lines[i].replace(/^(\s*)[\*_]\*?/g, '$1') + "</em>" + "</p>"
+    if(lines[i].match(/^(\s*)[_]|\*{2}/)){
+      lines[i] = "<p>" + "<em>" + lines[i].replace(/^(\s*)[_]|\*{2}?/g, '$1') + "</em>" + "</p>"
     }
-    else if(lines[i].trim()[0] === '*'){
-      lines[i] = "<p>" + "<strong>" + lines[i].replace(/^(\s*)[\*_]\*?/g, '$1') + "</strong>" + "</p/"
+    else if(lines[i].match(/^(\s*)\*{1}/)){
+      lines[i] = "<p>" + "<strong>" + lines[i].replace(/^(\s*)[\*_]\*?/g, '$1') + "</strong>" + "</p>"
     }
     else{
-      lines[i] = "<p>" + lines[i] + "</p/"
+      lines[i] = "<p>" + lines[i] + "</p>"
     }
   }
+  console.log("lines are " + lines.join('\n'))
   return lines.join('\n');
 }
