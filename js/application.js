25861b2e2fe165d136039c1eaaf75063e35010da
Josh Lim & Quyen Tran <pair+joshhlim.quyenttran@devbootcamp.com>
2017-06-06 09:54:21 -0700

    Incorporate transform function

--- js/application.js
+++ js/application.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
   $("textarea").keyup(function(event) {
-    console.log(event);
-    console.log($(this));
     var markdownInput = $(this).val();
-    console.log(markdownInput);
-    $("#preview").find("p").text(markdownInput);
+    var transformed = transform(markdownInput);
+    $("#preview").find("p").text(transformed);
   });
 })
